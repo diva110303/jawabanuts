0d0d2fae4d2c409061c75f47c1d917eb9be2e158
diva110303 <103906867+diva110303@users.noreply.github.com>
2022-04-18 16:01:59 +0700

    Add files via upload

---
+++ OOP.java
@@ -0,0 +1,10 @@
+
+public class OOP {
+    public static void main(String[] args) {
+        ModelInstagram user1 = new Instagram_Model("Dipa","Kalung","TEst",5,komenPost,"http//instragram/post")
+
+
+        ModelInstagram.post();
+        ModelInstagram.postingan();
+    }
+}
